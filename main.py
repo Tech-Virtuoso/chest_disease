a422563aba14513de46e770889136ec9a6a18c7b
Devendrasingh Solanki” <dsoulanki49@gmail>
2024-05-17 21:12:17 +0530

    Evalution added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Classifier import logger
 from Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from Classifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
-# from Classifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
+from Classifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -49,15 +49,15 @@ except Exception as e:
 
 
 
-# STAGE_NAME = "Evaluation stage"
-# try:
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    model_evalution = EvaluationPipeline()
-#    model_evalution.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+except Exception as e:
+        logger.exception(e)
+        raise e
 
